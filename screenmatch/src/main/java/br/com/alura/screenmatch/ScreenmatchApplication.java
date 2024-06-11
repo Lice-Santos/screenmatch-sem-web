9c5c8d8639332b8e38bb9ca4966f4a2361c44fc5
Alice Santos <86413853+Lice-Santos@users.noreply.github.com>
2024-06-10 22:03:46 -0300

    inicio projeto spring

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,29 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.Service.ConsumoAPI;
+import br.com.alura.screenmatch.Service.ConverterDados;
+import br.com.alura.screenmatch.model.DadosSerie;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner { // significa que permitirá algum,as chamadas
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoAPI consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+//		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
+//		System.out.println(json);
+		ConverterDados conversor = new ConverterDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println("--------Depois das conversões com json-------------");
+		System.out.println(dados);
+	}
+}
